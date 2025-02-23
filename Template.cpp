c28120610509548c36044f14c21957b547ed6dd9
Aash Patel <aash.patel434@gmail.com>
2025-02-23 23:36:48 +0000

    Init tempalte

---
+++ Template.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "test";
+}
+
+//Run code without creating an executable
+//g++ Template.cpp -o temp_program; ./temp_program; Remove-Item temp_program.exe
